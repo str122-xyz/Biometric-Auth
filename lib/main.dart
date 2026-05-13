0e4d5856b8b3b4e8133f9625df7498267656b69e
Satriahrl_ <satriaherlambang22@gmail.com>
2026-05-14 04:17:05 +0700

    routes: main
    routing login dan home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'pages/login_page.dart';
+import 'pages/home_page.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Biometric Auth Test',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+      },
+    );
+  }
+}
